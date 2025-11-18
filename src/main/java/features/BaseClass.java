39a0c2ee8b5ffeacaa4520f08cdfb1b1b849a4f5
sara <esraaty1@gmai.com>
2025-11-18 12:05:54 +0200

    Base Class Added

---
+++ src/main/java/features/BaseClass.java
@@ -0,0 +1,51 @@
+package org.example;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
+
+import java.util.Arrays;
+import java.util.List;
+
+public abstract class BaseClass {
+    protected WebDriver driver;
+
+    @BeforeMethod
+    public void Open() {
+        driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.navigate().to("https://www.saucedemo.com/");
+    }
+
+    @Test(dataProvider = "usersProvider")
+    protected void login(String username) {
+        driver.findElement(By.id("user-name")).sendKeys(username);
+        driver.findElement(By.id("password")).sendKeys("secret_sauce");
+        driver.findElement(By.id("login-button")).click();
+    }
+
+    @AfterMethod
+    public void teardown() {
+        if (driver != null) {
+            driver.quit();
+        }
+    }
+    public List<String> users = Arrays.asList(
+            "standard_user",
+            "locked_out_user",
+            "problem_user",
+            "performance_glitch_user",
+            "error_user",
+            "visual_user"
+    );
+
+    @DataProvider(name = "usersProvider")
+    public Object[] userProvider() {
+        return users.toArray();
+    }
+}
+
